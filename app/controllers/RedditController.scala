151a9a5ae017af7923a76b568a1b1f5962b34f2d
malcu89 <malcu89@gmail.com>
2016-06-15 00:00:09 +0200

    + get custom reddit json

---
+++ app/controllers/RedditController.scala
@@ -0,0 +1,38 @@
+package controllers
+
+import javax.inject._
+
+import play.api.mvc._
+
+import play.api.data._
+import play.api.data.Forms._
+
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
+
+import play.api.libs.ws._
+import play.api.libs.concurrent.Execution.Implicits._
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class RedditController @Inject()(ws: WSClient) extends Controller {
+
+  val userForm = Form(
+    mapping(
+      "tag" -> text
+    )(UserData.apply)(UserData.unapply)
+  )
+
+  def index = Action {
+    val uf = UserData("politics");
+    Ok(views.html.index(userForm.fill(uf)))
+  }
+
+  def userPost = Action.async(parse.form(userForm)) { implicit request =>
+    ws.url("https://www.reddit.com/r/" + request.body.tag + "/top.json").get().map(response => Ok(views.html.getReddit(response.body)))
+  }
+}
+
+case class UserData(tag: String)
